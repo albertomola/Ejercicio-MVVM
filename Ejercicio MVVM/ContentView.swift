7f012147b0b3e8b54c6e50e3512c34183295091a
albertomola <pacmonkeykong@gmail.com>
2024-04-18 15:53:42 +0200

    Initial Commit

---
+++ Ejercicio MVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ejercicio MVVM
+//
+//  Created by alberto on 18/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ejercicio MVVM/Ejercicio_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ejercicio_MVVMApp.swift
+//  Ejercicio MVVM
+//
+//  Created by alberto on 18/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Ejercicio_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
